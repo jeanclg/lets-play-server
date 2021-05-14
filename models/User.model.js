2351d14bf822b0a3d7e314c3075f94177a48ace7
Jean Garcia <jean_clg@hotmail.com>
2021-05-14 15:53:38 -0300

    finish user

--- models/User.model.js
+++ models/User.model.js
@@ -20,6 +20,11 @@ const UserSchema = new Schema({
   gamesList: [String],
   receivedMessages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
   uploadedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+  image_url: {
+    type: String,
+    default:
+      "https://uwosh.edu/deanofstudents/wp-content/uploads/sites/156/2019/02/profile-default.jpg",
+  },
 });
 
 const UserModel = model("User", UserSchema);
