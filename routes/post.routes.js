8a99303485ba47ed65d52f249e6aedcf82b3158b
Jean Garcia <jean_clg@hotmail.com>
2021-05-14 12:38:08 -0300

    update post-routes

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const PostModel = require("../models/Post.model");
-const UserModel = require("../models/User.model")
+const UserModel = require("../models/User.model");
 
 // Crud (CREATE) - HTTP POST
 // Criar um novo usuário
@@ -10,17 +10,16 @@ router.post("/user/:id/post", async (req, res) => {
   console.log(req.body);
 
   try {
-
     // Salva os dados de usuário no banco de dados (MongoDB) usando o body da requisição como parâmetro
     const result = await PostModel.create({
       ...req.body,
     });
 
     const user = await UserModel.findOneAndUpdate(
-        { _id: req.params.id },
-        { $push: { uploadedPosts : result._id } },
-        { new: true }
-      );
+      { _id: req.params.id },
+      { $push: { uploadedPosts: result._id } },
+      { new: true }
+    );
 
     // Responder o usuário recém-criado no banco para o cliente (solicitante). O status 201 significa Created
     return res.status(201).json(result);
@@ -31,33 +30,30 @@ router.post("/user/:id/post", async (req, res) => {
   }
 });
 
-
-
-
-// cRud (Read): Rota para listar todos os quartos
+// cRud (Read): Rota para listar todos os posts
 router.get("/post", async (req, res) => {
-    try {
-      // O find() sem filtros traz todos os documentos da collection
-      const posts = await PostModel.find();
-      console.log(post);
-  
-      // O status 200 é um status genérico de sucesso (OK)
-      return res.status(200).json(post);
-    } catch (err) {
-      return res.status(500).json({ msg: err });
-    }
-  });
-  
-  router.get("/post/:id", async (req, res) => {
-    try {
-      const post = await PostModel.findOne({ _id: req.params.id })
-      console.log(post);
-  
-      // O status 200 é um status genérico de sucesso (OK)
-      return res.status(200).json(room);
-    } catch (err) {
-      return res.status(500).json({ msg: err });
-    }
-  });
+  try {
+    // O find() sem filtros traz todos os documentos da collection
+    const posts = await PostModel.find();
+    console.log(posts);
+
+    // O status 200 é um status genérico de sucesso (OK)
+    return res.status(200).json(posts);
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const post = await PostModel.findOne({ _id: req.params.id });
+    console.log(post);
+
+    // O status 200 é um status genérico de sucesso (OK)
+    return res.status(200).json(post);
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
+});
 
 module.exports = router;
